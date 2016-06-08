53d0a5fc536eebe2a0053fb1e3a70cc2623498aa
Ryan Lijewski <RLijewski@users.noreply.github.com>
2016-06-07 21:04:37 -0500

    Added Styling and functionality to video controls
    Added hover styling for controls. Made the progress bar clickable.
    Added functionality that highlights the transcript as the video plays.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ window.onload = function() {
 
   // Video
   var video = document.getElementById("main-video");
+  var videoContainer = document.getElementById("video-container");
 
   // Buttons
   var playButton = document.getElementById("play-pause");
@@ -14,7 +15,7 @@ window.onload = function() {
   var formattedDuration = formatTime(video.duration);
 
   // Set initial videoTime
-  videoTime.innerHTML = formattedCurrentTime + "/" + formattedDuration;
+  videoTime.innerHTML = formattedCurrentTime + " / " + formattedDuration;
 
   // icons
   var playIcon = "<img src=icons/play-icon.png alt=play button icon/>";
@@ -22,9 +23,17 @@ window.onload = function() {
   var volumeOnIcon = "<img src=icons/volume-on-icon.png alt=volume on icon/>";
   var volumeOffIcon = "<img src=icons/volume-off-icon.png alt=volume off icon/>";
 
-  // Sliders
+  // Bars
   var progressBar = document.getElementById("progress-bar");
   var volumeBar = document.getElementById("volume-bar");
+  var prgress = document.getElementById("progress");
+
+  // Video Controls
+  var videoControls = document.getElementById("video-controls");
+  var controlBar = document.getElementById("control-bar");
+
+  // Transcript
+  var transcript = document.getElementById("transcript");
 
   // Format videoTime
   function formatTime(seconds) {
@@ -32,7 +41,6 @@ window.onload = function() {
     minutes = (minutes >= 10) ? minutes : "0" + minutes;
     seconds = Math.floor(seconds % 60);
     seconds = (seconds >= 10) ? seconds : "0" + seconds;
-    console.log(minutes + ":" + seconds);
     return minutes + ":" + seconds;
   }
 
@@ -81,37 +89,141 @@ window.onload = function() {
     }
   });
 
+  // Event listener for mouseover on videoContainer
+  videoContainer.addEventListener("mouseover", function() {
+    controlBar.style.height = "50px";
+    videoControls.style.display = "block";
+  });
+
+  // Event listener for mouseout on videoContainer
+  videoContainer.addEventListener("mouseout", function() {
+    controlBar.style.height = "15px";
+    videoControls.style.display = "none";
+  });
+
   // Event listener for the progressBar bar
-  progressBar.addEventListener("change", function() {
+  progressBar.addEventListener("click", function(e) {
+    var newWidth = e.offsetX;
+    // Update progress width
+    progress.style.width = newWidth + "px";
     // Calculate the new time
-    var time = video.duration * (progressBar.value / 100);
+    var time = video.duration * (newWidth / progressBar.offsetWidth);
     // Update the video time
     video.currentTime = time;
   });
 
   // Update the progressBar and videoTime as the video plays
   video.addEventListener("timeupdate", function() {
-    // Calculate the slider value
-    var value = (100 / video.duration) * video.currentTime;
-    // Update the slider value
-    progressBar.value = value;
-    // Updae videoTime value
-    formattedCurrentTime = formatTime(video.currentTime);
-    videoTime.innerHTML = formattedCurrentTime + "/" + formattedDuration;
-  });
+    // Get current video time
+    var time = video.currentTime;
+    // Calculate the progress value
+    var value = (100 / video.duration) * time;
+    // Update the progress value
+    progress.style.width = value + "%";
+    // Update videoTime value
+    formattedCurrentTime = formatTime(time);
+    videoTime.innerHTML = formattedCurrentTime + " / " + formattedDuration;
 
-  // Event listener for the volume bar
-  volumeBar.addEventListener("change", function() {
-    // Update the video volume
-    video.volume = volumeBar.value;
-    // Change volume icon
-    if(volumeBar.value == 0){
-      // Update the button icon to volumeOffIcon
-      muteToggle.innerHTML = volumeOffIcon;
+    // Update transcript text color
+    var transcriptSection = transcript.children;
+
+
+      console.log(transcriptSection.length);
+
+    if (time >= 0.240 && time < 4.130){
+      transcriptSection[0].style.color = "#ff9900";
     } else {
-      // Update the button icon to volumeOnIcon
-      muteToggle.innerHTML = volumeOnIcon;
+      transcriptSection[0].style.color = "black";
+    }
+
+    if (time >= 4.130 && time < 7.535){
+      transcriptSection[1].style.color = "#ff9900";
+    } else {
+      transcriptSection[1].style.color = "black";
+    }
+
+    if (time >= 7.535 && time < 11.270){
+      transcriptSection[2].style.color = "#ff9900";
+    } else {
+      transcriptSection[2].style.color = "black";
+    }
+
+    if (time >= 11.270 && time < 13.960){
+      transcriptSection[3].style.color = "#ff9900";
+    } else {
+      transcriptSection[3].style.color = "black";
+    }
+
+    if (time >= 13.960 && time < 17.940){
+      transcriptSection[4].style.color = "#ff9900";
+    } else {
+      transcriptSection[4].style.color = "black";
+    }
+
+    if (time >= 17.940 && time < 22.370){
+      transcriptSection[5].style.color = "#ff9900";
+    } else {
+      transcriptSection[5].style.color = "black";
+    }
+
+    if (time >= 22.370 && time < 26.880){
+      transcriptSection[6].style.color = "#ff9900";
+    } else {
+      transcriptSection[6].style.color = "black";
+    }
+
+    if (time >= 26.880 && time < 30.920){
+      transcriptSection[7].style.color = "#ff9900";
+    } else {
+      transcriptSection[7].style.color = "black";
+    }
+
+    if (time >= 32.100 && time < 34.730){
+      transcriptSection[8].style.color = "#ff9900";
+    } else {
+      transcriptSection[8].style.color = "black";
+    }
+
+    if (time >= 34.730 && time < 39.430){
+      transcriptSection[9].style.color = "#ff9900";
+    } else {
+      transcriptSection[9].style.color = "black";
+    }
+
+    if (time >= 39.430 && time < 41.190){
+      transcriptSection[10].style.color = "#ff9900";
+    } else {
+      transcriptSection[10].style.color = "black";
+    }
+
+    if (time >= 42.350 && time < 46.300){
+      transcriptSection[11].style.color = "#ff9900";
+    } else {
+      transcriptSection[11].style.color = "black";
+    }
+
+    if (time >= 46.300 && time < 49.270){
+      transcriptSection[12].style.color = "#ff9900";
+    } else {
+      transcriptSection[12].style.color = "black";
+    }
+
+    if (time >= 49.270 && time < 53.760){
+      transcriptSection[13].style.color = "#ff9900";
+    } else {
+      transcriptSection[13].style.color = "black";
+    }
+
+    if (time >= 53.760 && time < 57.780){
+      transcriptSection[14].style.color = "#ff9900";
+    } else {
+      transcriptSection[14].style.color = "black";
+    }
+
+    if (time >= 57.780 && time < 60.150){
+      transcriptSection[15].style.color = "#ff9900";
+    } else {
+      transcriptSection[15].style.color = "black";
     }
   });
-
 }
